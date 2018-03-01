332c2e9987650f96d4ff69cd9776f7b5efcb3897
Pablo Nevares <pablo@nevares.com>
2018-02-28 21:07:19 -0700

    goals

---
+++ index.js
@@ -0,0 +1,9 @@
+// import twit
+
+// set up twit
+
+// listen for new tweets from waypoint-affiliated accounts
+
+// send those new tweets through morae
+
+// if a tweet can be made into a haiku, quote tweet and tweet the haiku from @WaypointHaikus
